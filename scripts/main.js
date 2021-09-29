7dc587602f326e0f7c51bcca2e13a0509cb1f56d
rhit-martina9 <89958644+rhit-martina9@users.noreply.github.com>
2021-09-29 16:13:41 -0400

    javascript interactivity

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+const scrollElementsLeft = document.querySelectorAll(".js-scroll-left");
+const scrollElementsRight = document.querySelectorAll(".js-scroll-right");
+
+const elementInView = (el, percentageScroll = 100) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll/100))
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimationLeft = () => {
+  scrollElementsLeft.forEach((el) => {
+    if (elementInView(el, 50)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimationLeft();
+});
+
+
+const handleScrollAnimationRight = () => {
+  scrollElementsRight.forEach((el) => {
+    if (elementInView(el, 15)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimationRight();
+});
